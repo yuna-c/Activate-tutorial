9a4a0385555bba2e5d0f670e54021dcdc0bbc38a
yuna-c <yuna744@gmail.com>
2023-11-13 11:06:36 +0900

    1113 이벤트 함수 분리

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 const main = document.querySelector('main'); //메인 찾기
 const btns = main.querySelectorAll('li');
 const boxs = main.querySelectorAll('article');
-console.log(boxs.length);
-console.log(btns.length);
 
+// console.log(boxs);
+// console.log(btns);
+
+// 버튼 클릭 이벤트문
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
-		//인수 콜백함수
-		//직전의 모든 요소 초기화
-		btns.forEach((el) => el.classList.remove('on'));
-		//클릭한 순번에 해당하는 버튼만 활성화
-		btns[idx].classList.add('on');
+		// activation(btns, idx);
+		// activation(boxs, idx);
 
-		boxs.forEach((el) => el.classList.remove('on'));
-		boxs[idx].classList.add('on');
+		[btns, boxs].forEach((el) => activation(el, idx));
 	});
 });
+
+// 인수로 그룹 선택자와, 순번을 받아서
+// 순번의 요소만 활성화 시켜주는 함수
+function activation(arr, idx) {
+	arr.forEach((el) => el.classList.remove('on'));
+	arr[idx].classList.add('on');
+}
