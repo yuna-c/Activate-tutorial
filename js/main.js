b3344feab72796ecf540846179106d0a96e6b286
yuna-c <yuna744@gmail.com>
2023-11-13 11:51:14 +0900

    1113 currentTarget, preventDefault로 기본기능 막기

--- js/main.js
+++ js/main.js
@@ -7,18 +7,21 @@ const boxs = main.querySelectorAll('article');
 
 // 버튼 클릭 이벤트문
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
-		// activation(btns, idx);
-		// activation(boxs, idx);
-		// 만약 클릭한 버튼에 class on이 있다면 return으로 함수 실행 중지
-		// 만약 on이 없다면 해당 조건문 무시되면서 정상적으로 activation 함수 호출
-		if (btns[idx].classList.contains('on')) return;
+	btn.addEventListener('click', (e) => {
+		/* 
+    console.log(e);
+		console.log('currentTarget', e.currentTarget);
+		console.log('target', e.target); 
+    */
+		// e.currentTarget : js이벤트 구문에 연결되어 있는 선택, 클릭한 자기 자신의 요소
+		// e.target : 실제 화면상에서 이벤트가 발생한 대상 선택, 동적 이벤트 요소 발생 시킬 때 사용
+		// 만약 연결되어 있는 선택자가 a의 링크이동처럼 특정 기능이 있는 요소면 해당기능 막아야 될 필요있음 (링크기능 막고 스크립트 실행 시킴)
+		e.preventDefault();
+		if (/*btns[idx]*/ e.currentTarget.classList.contains('on')) return;
 		[btns, boxs].forEach((el) => activation(el, idx));
 	});
 });
 
-// 인수로 그룹 선택자와, 순번을 받아서
-// 순번의 요소만 활성화 시켜주는 함수
 function activation(arr, idx) {
 	console.log('activation');
 	arr.forEach((el) => el.classList.remove('on'));
