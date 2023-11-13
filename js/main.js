c5653c18682ad5d9bd2df0c7c043b3ddeac47aa5
yuna-c <yuna744@gmail.com>
2023-11-13 11:34:05 +0900

    1113 활성화 버튼 클릭시 재함수 호출 방지

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@ btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
 		// activation(btns, idx);
 		// activation(boxs, idx);
-
+		// 만약 클릭한 버튼에 class on이 있다면 return으로 함수 실행 중지
+		// 만약 on이 없다면 해당 조건문 무시되면서 정상적으로 activation 함수 호출
+		if (btns[idx].classList.contains('on')) return;
 		[btns, boxs].forEach((el) => activation(el, idx));
 	});
 });
@@ -18,6 +20,7 @@ btns.forEach((btn, idx) => {
 // 인수로 그룹 선택자와, 순번을 받아서
 // 순번의 요소만 활성화 시켜주는 함수
 function activation(arr, idx) {
+	console.log('activation');
 	arr.forEach((el) => el.classList.remove('on'));
 	arr[idx].classList.add('on');
 }
