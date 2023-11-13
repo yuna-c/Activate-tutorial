4b00f94a044cf925c729fcb433767462d0bcebd0
yuna-c <yuna744@gmail.com>
2023-11-13 10:58:29 +0900

    1113 tab메뉴 기본 로직 완성

--- js/main.js
+++ js/main.js
@@ -8,15 +8,11 @@ btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => {
 		//인수 콜백함수
 		//직전의 모든 요소 초기화
-		btns.forEach((btn) => btn.classList.remove('on'));
+		btns.forEach((el) => el.classList.remove('on'));
 		//클릭한 순번에 해당하는 버튼만 활성화
 		btns[idx].classList.add('on');
+		boxs.forEach((el) => el.classList.remove('on'));
+		//클릭한 순번에 해당하는 버튼만 활성화
+		boxs[idx].classList.add('on');
 	});
 });
-
-// boxs.forEach((box, idx) => {
-// 	box.addEventListener('click', () => {
-// 		boxs.forEach((box) => box.classList.remove('on'));
-// 		boxs[idx].classList.add('on');
-// 	});
-// });
